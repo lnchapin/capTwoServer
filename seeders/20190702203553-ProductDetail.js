12f03f5464e7d467de280386b2d231274c75f7ad
Lindsay Chapin <lnchapin@gmail.com>
2019-07-21 15:17:41 -0600

    adding stock for stripe integration

--- seeders/20190702203553-ProductDetail.js
+++ seeders/20190702203553-ProductDetail.js
@@ -15,31 +15,31 @@ module.exports = {
     return queryInterface.bulkInsert('ProductDetails', [
       {
         description: 'Sturdy rain boots in blue',
-        quantity: 0,
+        quantity: 100,
         manufacture: 'Wellington',
         product_id: 1
       },
       {
         description: 'Khaki tool belt with four pockets',
-        quantity: 0,
+        quantity: 100,
         manufacture: 'gardeners',
         product_id: 2
       },
       {
         description: 'Southern Ag OMRI tested neem oil. Broad spectrum fungicide/insecticde/miticide',
-        quantity: 0,
+        quantity: 100,
         manufacture: 'Southern Ag',
         product_id: 3
       },
       {
         description: 'A comprehensive home preserving guide for the creative cook by Sherri Brooks Vinton',
-        quantity: 0,
+        quantity: 100,
         manufacture: 'Storey Publishing',
         product_id: 4
       },
       {
         description: 'A guide to home-scale permaculture by Toby Hemenway. The Second Edition.',
-        quantity: 0,
+        quantity: 100,
         manufacture: 'Chelsea Green Publishing',
         product_id: 5
       }
